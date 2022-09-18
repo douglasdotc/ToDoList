422039ca2674dea8c8f8ed671f26efe643cf46cd
Douglas <douglasdothnc@gmail.com>
2022-09-18 15:26:00 +0100

    [DEV] Add log to backend and Angular frontend
    Use Lombok to add logs in controllers and format proper HTTP responses from backend to frontend
    
    Add Angular initial frontend project folder

--- backend/src/main/java/ToDoList/backend/service/ItemService.java
+++ backend/src/main/java/ToDoList/backend/service/ItemService.java
@@ -2,10 +2,10 @@ package TodoList.backend.service;
 
 import TodoList.backend.model.Item;
 import TodoList.backend.repository.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Objects;
@@ -13,25 +13,25 @@ import java.util.Objects;
 // This class holds all the business logic for Item
 // TODO: CRUD an item in a board require updating the last edited entry in Board
 @Service
+@RequiredArgsConstructor // Constructor with Dependency injection
+@Transactional // This annotation frees the function from calling queries (E.g. SELECT b from Board b WHERE ...)
+@Slf4j // Logs
 public class ItemService {
     private final ItemRepository itemRepository; // Interface that use to access the item database
 
-    @Autowired
-    public ItemService(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }
-
     public List<Item> getItems() {
+        log.info("Fetching all items");
         return itemRepository.findAll();
     }
 
-    public void createItem(Item item) {
+    public Item createItem(Item item) {
         // Different board can have item with the same title and content
-        itemRepository.save(item);
+        log.info("saving new item ID: {}", item.getItemId());
+        return itemRepository.save(item);
     }
 
-    @Transactional
-    public void updateItem(Long itemId, String title, String content) {
+    public Boolean updateItem(Long itemId, String title, String content) {
+        log.info("Updating item with ID: {}", itemId);
         Item item = itemRepository.findById(itemId).orElseThrow(
             () -> new IllegalStateException("[PUT] Item with id: " + itemId + " does not exist")
         );
@@ -43,13 +43,16 @@ public class ItemService {
         if (content != null && content.length() > 0 && !Objects.equals(item.getContent(), content)) {
             item.setContent(content);
         }
+        return true;
     }
 
-    public void deleteItem(Long itemId) {
+    public Boolean deleteItem(Long itemId) {
+        log.info("Deleting item with ID: {}", itemId);
         boolean isExist = itemRepository.existsById(itemId);
         if (!isExist) {
             throw new IllegalStateException("[DELETE] Item with id: " + itemId + " does not exist.");
         }
         itemRepository.deleteById(itemId);
+        return true;
     }
 }
